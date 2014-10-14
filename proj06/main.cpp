03d9d62b66fab94f5255541a0151aac0af21abdc
Bryan J Kars <kars@host-101134.dhcp.egr.msu.edu>
2014-10-14 10:39:23 -0400

    Initial Commit

---
+++ proj06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proj06
+//
+//  Created by Bryan J Kars on 10/14/14.
+//  Copyright (c) 2014 Bryan J Kars. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
